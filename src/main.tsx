ed13387f18703260def4d01e23dff3bd7e71b9ba
Erkhemtugs <erhemevlee@gmail.com>
2025-06-02 21:56:31 -0500

    added google analytic to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+if (import.meta.env.PROD) {
+  // Load Google Analytics script
+  const script1 = document.createElement('script');
+  script1.async = true;
+  script1.src = 'https://www.googletagmanager.com/gtag/js?id=G-9BQGL55N8H';
+  document.head.appendChild(script1);
+
+  // Inline GA config
+  const script2 = document.createElement('script');
+  script2.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-9BQGL55N8H');
+  `;
+  document.head.appendChild(script2);
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
